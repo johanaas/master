f990a3e84d9bcf7c979574bca6579010c523519e
kamidtli <kmidtlid3@gmail.com>
2022-03-10 21:27:39 +0100

    Fix model loading

--- models/__init__.py
+++ models/__init__.py
@@ -3,8 +3,8 @@ from .Resnet101 import ResnetModel101
 
 def get_model(name):
   if name == "resnet50":
-    return ResnetModel50
+    return ResnetModel50()
   elif name == "resnet101":
-    return ResnetModel101
+    return ResnetModel101()
   else:
     raise ValueError("No model with name:", name)
