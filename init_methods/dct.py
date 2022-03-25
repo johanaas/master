6463aace851fa6dd01a8f3cbe2afc62785cc77bc
Kim Midtlid <kmidtlid3@gmail.com>
2022-03-25 10:19:02 +0100

    Add simple mask method for adversarial check

--- init_methods/dct.py
+++ init_methods/dct.py
@@ -2,9 +2,43 @@ import numpy as np
 from scipy.fftpack import dct, idct
 import cv2
 from utils import decision_function
+from matplotlib import pyplot as plt
 
 def get_dct_image(orig_img, model, params, band_width=50, step_size=40, perturbation_factor=1):
 
+    y, u, v = get_yuv_from_rgb(orig_img)
+    random_noise = np.random.uniform(0, 1, y.shape)
+
+    dct_coeffs = dct2(y)
+
+    coeffs_max = np.max(dct_coeffs)
+
+    epsilon = 0.01
+
+    for i in range(5):
+        thresh = coeffs_max * epsilon**i
+        mask = (np.abs(dct_coeffs) < thresh)
+
+        perturbed_coeffs = np.copy(dct_coeffs)
+        perturbed_coeffs[mask] = random_noise[mask]
+
+        perturbed_img = get_rgb_from_yuv([idct2(perturbed_coeffs), u, v])
+
+        fig, axs = plt.subplots(2, 1)
+
+        axs[0].imshow(perturbed_img)
+        axs[1].imshow(perturbed_coeffs)
+        plt.show()
+
+        if model.predict(perturbed_img): # TODO: change to actual model. True if adversarial
+            return perturbed_img
+        
+        break
+
+    print("\n\n\nImage is not adversarial. Throwing exception\n\n\n")
+    raise ValueError("Could not create adversarial image")
+
+    """
     # DCT is faster and easier in 2D, thus we use
     # the limunance component of the YUV format
     img, u, v = get_yuv_from_rgb(orig_img)
@@ -27,6 +61,7 @@ def get_dct_image(orig_img, model, params, band_width=50, step_size=40, perturba
 
     print("Could not find adversarial image through DCT with step size band_width {} and step size {}.".format(band_width, step_size))
     return None
+    """
 
 def get_diag_masks(img_width, band_width=1, step_size=1):
     mask_container = np.ones((img_width, img_width))
@@ -67,9 +102,8 @@ def get_yuv_from_rgb(img):
     y, u, v = cv2.split(img_yuv)
     return y, u, v
 
-def get_rgb_from_yuv(y, u, v):
-    # img_yuv = cv2.merge((y, u, v))
-    img_yuv = np.dstack([y, u, v])
+def get_rgb_from_yuv(channels):
+    img_yuv = np.dstack(channels)
     img = cv2.cvtColor(np.float32(img_yuv), cv2.COLOR_YUV2RGB)
     return img
 
